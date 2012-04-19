f5af8a9afd2520b1d5ecf760cdbdba091818997e
Donearm <forod.g@gmail.com>
2012-04-19 22:36:30 +0200

    simplified luakit user agent string

--- .config/luakit/globals.lua
+++ .config/luakit/globals.lua
@@ -19,9 +19,8 @@ globals = {
 local _, arch = luakit.spawn_sync("uname -sm")
 -- Only use the luakit version if in date format (reduces identifiability)
 local lkv = string.match(luakit.version, "^(%d+.%d+.%d+)")
-globals.useragent = string.format("Mozilla/5.0 (%s) AppleWebKit/%s+ (KHTML, like Gecko) WebKitGTK+/%s luakit%s",
-    string.sub(arch, 1, -2), luakit.webkit_user_agent_version,
-    luakit.webkit_version, (lkv and ("/" .. lkv)) or "")
+globals.useragent = string.format("Mozilla/5.0 (%s) AppleWebKit/%s+ (KHTML, like Gecko) WebKitGTK+",
+    string.sub(arch, 1, -2), luakit.webkit_user_agent_version)
 
 -- Search common locations for a ca file which is used for ssl connection validation.
 local ca_files = {
