bf90e394828412e5ad15383829211ef65a28811d
Gianluca Fiore <forod.g@gmail.com>
2012-06-17 19:32:22 +0200

    updated binds.lua to latest git

--- .config/luakit/binds.lua
+++ .config/luakit/binds.lua
@@ -12,7 +12,6 @@ local strip, split = lousy.util.string.strip, lousy.util.string.split
 
 -- Globals or defaults that are used in binds
 local scroll_step = globals.scroll_step or 20
-local more, less = "+"..scroll_step.."px", "-"..scroll_step.."px"
 local zoom_step = globals.zoom_step or 0.1
 
 -- Add binds to a mode
@@ -110,54 +109,51 @@ add_binds("normal", {
 
     key({},          "i",           function (w) w:set_mode("insert")  end),
     key({},          ":",           function (w) w:set_mode("command") end),
+
+    -- Scrolling
+    key({},          "j",           function (w) w:scroll{ yrel =  scroll_step } end),
+    key({},          "k",           function (w) w:scroll{ yrel = -scroll_step } end),
+    key({},          "h",           function (w) w:scroll{ xrel = -scroll_step } end),
+    key({},          "l",           function (w) w:scroll{ xrel =  scroll_step } end),
+    key({},          "Down",        function (w) w:scroll{ yrel =  scroll_step } end),
+    key({},          "Up",          function (w) w:scroll{ yrel = -scroll_step } end),
+    key({},          "Left",        function (w) w:scroll{ xrel = -scroll_step } end),
+    key({},          "Right",       function (w) w:scroll{ xrel =  scroll_step } end),
+
+    key({},          "^",           function (w) w:scroll{ x =  0 } end),
+    key({},          "$",           function (w) w:scroll{ x = -1 } end),
+    key({},          "0",           function (w, m)
+                                        if not m.count then w:scroll{ y = 0 } else return false end
+                                    end),
+
+    key({"Control"}, "e",           function (w) w:scroll{ yrel =  scroll_step } end),
+    key({"Control"}, "y",           function (w) w:scroll{ yrel = -scroll_step } end),
+    key({"Control"}, "d",           function (w) w:scroll{ ypagerel =  0.5 } end),
+    key({"Control"}, "u",           function (w) w:scroll{ ypagerel = -0.5 } end),
+    key({"Control"}, "f",           function (w) w:scroll{ ypagerel =  1.0 } end),
+    key({"Control"}, "b",           function (w) w:scroll{ ypagerel = -1.0 } end),
+
+    key({},          "space",       function (w) w:scroll{ ypagerel =  1.0 } end),
+    key({"Shift"},   "space",       function (w) w:scroll{ ypagerel = -1.0 } end),
+    key({},          "BackSpace",   function (w) w:scroll{ ypagerel = -1.0 } end),
+
+    key({},          "Page_Down",   function (w) w:scroll{ ypagerel =  1.0 } end),
+    key({},          "Page_Up",     function (w) w:scroll{ ypagerel = -1.0 } end),
+
+    key({},          "Home",        function (w) w:scroll{ y =  0 } end),
+    key({},          "End",         function (w) w:scroll{ y = -1 } end),
+
     -- Download and play flash videos
     key({},          "v",           function (w) local uri = (w:get_current() or {}).uri if uri then
                                         luakit.spawn("urxvt -e get_flash_videos -y -r high -p '" .. uri .. "'") end
                                     end),
 
-    -- Scrolling
-    key({},          "j",           function (w) w:scroll{ y = more    } end),
-    key({},          "k",           function (w) w:scroll{ y = less    } end),
-    key({},          "h",           function (w) w:scroll{ x = less    } end),
-    key({},          "l",           function (w) w:scroll{ x = more    } end),
-    key({},          "^",           function (w) w:scroll{ x = "0%"    } end),
-    key({},          "$",           function (w) w:scroll{ x = "100%"  } end),
-    key({"Control"}, "e",           function (w) w:scroll{ y = more    } end),
-    key({"Control"}, "y",           function (w) w:scroll{ y = less    } end),
---    key({"Control"}, "d",           function (w) w:scroll{ y = "+0.5p" } end),
---    key({"Control"}, "u",           function (w) w:scroll{ y = "-0.5p" } end),
---    key({"Control"}, "f",           function (w) w:scroll{ y = "+1.0p" } end),
---    key({"Control"}, "b",           function (w) w:scroll{ y = "-1.0p" } end),
---    key({},          "space",       function (w) w:scroll{ y = "+1.0p" } end),
---    key({"Shift"},   "space",       function (w) w:scroll{ y = "-1.0p" } end),
---    key({},          "BackSpace",   function (w) w:scroll{ y = "-1.0p" } end),
-    key({"Control"}, "d",           function (w) w:scroll{ y = string.format("+%.1fp", 0.5) } end),
-    key({"Control"}, "u",           function (w) w:scroll{ y = string.format("-%.1fp", 0.5) } end),
-    key({"Control"}, "f",           function (w) w:scroll{ y = string.format("+%.1fp", 1.0) } end),
-    key({"Control"}, "b",           function (w) w:scroll{ y = string.format("-%.1fp", 1.0) } end),
-    key({},          "space",       function (w) w:scroll{ y = string.format("+%.1fp", 1.0) } end),
-    key({"Shift"},   "space",       function (w) w:scroll{ y = string.format("-%.1fp", 1.0) } end),
-    key({},          "BackSpace",   function (w) w:scroll{ y = string.format("-%.1fp", 1.0) } end),
-
     -- Specific scroll
-    buf("^gg$",                     function (w, b, m) w:scroll{ y = m.count.."%" } end, {count = 0}),
-    buf("^G$",                      function (w, b, m) w:scroll{ y = m.count.."%" } end, {count = 100}),
+    buf("^gg$",                     function (w, b, m) w:scroll{ ypct = m.count } end, {count = 0}),
+    buf("^G$",                      function (w, b, m) w:scroll{ ypct = m.count } end, {count = 100}),
+    buf("^%%$",                     function (w, b, m) w:scroll{ ypct = m.count } end),
 
     -- Traditional scrolling commands
-    key({},          "Down",        function (w) w:scroll{ y = more    } end),
-    key({},          "Up",          function (w) w:scroll{ y = less    } end),
-    key({},          "Left",        function (w) w:scroll{ x = less    } end),
-    key({},          "Right",       function (w) w:scroll{ x = more    } end),
---    key({},          "Page_Down",   function (w) w:scroll{ y = "+1.0p" } end),
-    key({},          "Page_Down",   function (w) w:scroll{ y = string.format("+%.1fp", 1.0) } end),
---    key({},          "Page_Up",     function (w) w:scroll{ y = "-1.0p" } end),
-    key({},          "Page_Up",     function (w) w:scroll{ y = string.format("-%.1fp", 1.0) } end),
-    key({},          "Home",        function (w) w:scroll{ y = "0%"    } end),
-    key({},          "End",         function (w) w:scroll{ y = "100%"  } end),
-    key({},          "$",           function (w) w:scroll{ x = "100%"  } end),
-    key({},          "0",           function (w, m)
-                                        if not m.count then w:scroll{ y = "0%" } else return false end
-                                    end),
 
     -- Zooming
     key({},          "+",           function (w, m)    w:zoom_in(zoom_step  * m.count)       end, {count=1}),
@@ -226,6 +222,8 @@ add_binds("normal", {
     key({"Shift","Control"}, "Tab", function (w)       w:prev_tab() end),
     buf("^gT$",                     function (w, b, m) w:prev_tab(m.count) end, {count=1}),
     buf("^gt$",                     function (w, b, m) if not w:goto_tab(m.count) then w:next_tab() end end, {count=0}),
+    buf("^g0$",                     function (w)       w:goto_tab(1)  end),
+    buf("^g$$",                     function (w)       w:goto_tab(-1) end),
 
     key({"Control"}, "t",           function (w)    w:new_tab(globals.homepage) end),
     key({"Control"}, "w",           function (w)    w:close_tab()       end),
@@ -244,7 +242,7 @@ add_binds("normal", {
 
     key({},          "r",           function (w) w:reload() end),
     key({},          "R",           function (w) w:reload(true) end),
-    key({"Control"}, "c",           function (w) w:stop() end),
+    key({"Control"}, "c",           function (w) w.view:stop() end),
 
     -- Config reloading
     key({"Control", "Shift"}, "R",  function (w) w:restart() end),
@@ -260,7 +258,7 @@ add_binds("normal", {
 
 add_binds("insert", {
     key({"Control"}, "z",           function (w) w:set_mode("passthrough") end),
-    key({"Control"},  "e",       function (w)
+    key({"Control", "Shift"},  "e",       function (w)
         local editor = "urxvt -e vi -c 'set spell'" 
         local dir = "/home/gianluca/" 
         local time = os.time()
@@ -344,7 +342,8 @@ add_cmds({
  -- cmd({command, alias1, ...}, function (w, arg, opts) .. end, opts),
  -- cmd("co[mmand]",            function (w, arg, opts) .. end, opts),
     cmd("c[lose]",              function (w) w:close_tab() end),
-    cmd("print",                function (w) w:eval_js("print()", "rc.lua") end),
+    cmd("print",                function (w) w.view:eval_js("print();") end),
+    cmd("stop",                 function (w) w.view:stop() end),
     cmd("reload",               function (w) w:reload() end),
     cmd("restart",              function (w) w:restart() end),
     cmd("write",                function (w) w:save_session() end),
@@ -356,7 +355,16 @@ add_cmds({
     cmd("o[pen]",               function (w, a) w:navigate(w:search_open(a)) end),
     cmd("t[abopen]",            function (w, a) w:new_tab(w:search_open(a)) end),
     cmd("w[inopen]",            function (w, a) window.new{w:search_open(a)} end),
-    cmd({"javascript",   "js"}, function (w, a) w:eval_js(a, "javascript") end),
+    cmd({"javascript", "js"}, function (w, a) w.view:eval_js(a) end),
+
+    -- Tab manipulation commands
+    cmd("tab",                  function (w, a) w:new_tab() w:run_cmd(":" .. a) end),
+    cmd("tabd[o]",              function (w, a) w:each_tab(function (v) w:run_cmd(":" .. a) end) end),
+    cmd("tabdu[plicate]",       function (w)    w:new_tab(w.view.history) end),
+    cmd("tabfir[st]",           function (w)    w:goto_tab(1) end),
+    cmd("tabl[ast]",            function (w)    w:goto_tab(-1) end),
+    cmd("tabn[ext]",            function (w)    w:next_tab() end),
+    cmd("tabp[revious]",        function (w)    w:prev_tab() end),
 
     cmd("q[uit]",               function (w, a, o) w:close_win(o.bang) end),
     cmd({"viewsource",  "vs" }, function (w, a, o) w:toggle_source(not o.bang and true or nil) end),
@@ -376,7 +384,7 @@ add_cmds({
         local file = a or luakit.save_file("Save file", w.win, xdg.download_dir or '.', fname)
         if file then
             local fd = assert(io.open(file, "w"), "failed to open: " .. file)
-            local html = assert(w:eval_js("document.documentElement.outerHTML", "dump"), "Unable to get HTML")
+            local html = assert(w.view:eval_js("document.documentElement.outerHTML"), "Unable to get HTML")
             assert(fd:write(html), "unable to save html")
             io.close(fd)
             w:notify("Dumped HTML to: " .. file)
